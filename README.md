cf5ac29a7ceddbf6bfc5fffbea73a20a85bbd704
552020 <stefanolombardo@posteo.de>
2021-09-10 07:43:31 +0200

    add slider to sections 1

--- README.md
+++ README.md
@@ -5,12 +5,15 @@ Messing around with the [intersection observer API](https://developer.mozilla.or
 1. sticky navigation bar popping up when the first section comes into view: check.
 2. sections fade-in: check.
 3. sections fade out: to do.
+4. a slider - without 'dots' and with only images for the section1: check.
 
 ## technologies
 
 HTML, CSS, JS
 
 ## online
+
+[here](https://intersection-o.netlify.app/)
 
 ## responsive
 
--- script.js
+++ script.js
@@ -31,25 +31,90 @@ headerObserver.observe(header);
 
 // sections fade-in
 
-
-const allSections = document.querySelectorAll('.section');
+const allSections = document.querySelectorAll(".section");
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
   console.log(entry);
 
   if (!entry.isIntersecting) return;
-  entry.target.classList.remove('section--hidden');
+  entry.target.classList.remove("section--hidden");
+  // entry.target.classList.add("to-hide");
   observer.unobserve(entry.target);
 };
 
-const sectionObserver = new IntersectionObserver(revealSection, {
+// const hideSection = function (entries, observer) {
+//   const [entry] = entries;
+//   console.log(entry.target.classList.value);
+//   console.log(typeof entry.target.classList);
+//   console.log(entry.target.classList.value.includes("to-hide"));
+//   if (!entry.isIntersecting && entry.target.classList.value.includes("to-hide"))
+//     return;
+
+//   entry.target.classList.add("section--hidden");
+// };
+
+const revealSectionObserver = new IntersectionObserver(revealSection, {
   root: null,
   threshold: 0.15,
 });
 
-allSections.forEach(section => {
-  sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+// const hideSectionObserver = new IntersectionObserver(hideSection, {
+//   root: null,
+//   threshold: 0.1,
+// });
+
+allSections.forEach((section) => {
+  revealSectionObserver.observe(section);
+  // hideSectionObserver.observe(section);
+  // section.classList.add("section--hidden");
 });
 
+// SLIDER
+// FADE-IN: DEACTIVATED
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+
+let curSlide = 0;
+
+let maxSlide = slides.length;
+
+// TEMPORARY START
+const slider = document.querySelector(".slider");
+
+// slider.style.transform = "scale(0.4) translateX(-800px";
+// slider.style.overflow = "visible";
+
+// TEMPORARY END ***
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+
+btnLeft.addEventListener("click", prevSlide);
